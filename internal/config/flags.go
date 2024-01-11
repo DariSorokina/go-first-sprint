9f3fa9f0b46145d6c34247885abd0dd8ff741109
Daria Sorokina <dariasorokina@MacBook-Pro-Daria.local>
2024-01-12 02:40:29 +0300

    iter4

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -8,7 +8,7 @@ var FlagRunAddr string
 var FlagBaseURL string
 
 func ParseFlags() {
-	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&FlagRunAddr, "a", ":8000", "address and port to run server")
 	flag.StringVar(&FlagBaseURL, "b", "http://localhost:8000/", "prefix for trimed URL")
 	flag.Parse()
 }
