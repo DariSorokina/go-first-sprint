ad2f8aae873c99fa3886345cc0563e4b08473d1e
Daria Sorokina <dariasorokina@MacBook-Pro-Daria.local>
2024-01-13 01:47:49 +0300

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var FlagRunAddr string
@@ -11,4 +12,11 @@ func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&FlagBaseURL, "b", "http://localhost:8080/", "prefix for trimed URL")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		FlagRunAddr = envRunAddr
+	}
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		FlagBaseURL = envBaseURL
+	}
 }
