b28a8e1b3e356e2546f156e4ae6f71887d60ba45
DariSorokina <d.sorokina1113@gmail.com>
2024-08-11 20:24:32 +0400

    iter19

---
+++ cmd/staticlint/testdata/test_failed.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/DariSorokina/go-first-sprint/internal/app"
+	"github.com/DariSorokina/go-first-sprint/internal/config"
+	"github.com/DariSorokina/go-first-sprint/internal/logger"
+	"github.com/DariSorokina/go-first-sprint/internal/server"
+	"github.com/DariSorokina/go-first-sprint/internal/storage"
+)
+
+func main() {
+	flagConfig := config.ParseFlags()
+
+	var l *logger.Logger
+	var err error
+	if l, err = logger.CreateLogger(flagConfig.FlagLogLevel); err != nil {
+		log.Fatal("Failed to create logger:", err)
+	}
+
+	storage, err := storage.SetStorage(flagConfig, l)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer storage.Close()
+
+	app := app.NewApp(storage, l)
+	serv := server.NewServer(app, flagConfig, l)
+
+	if err := server.Run(serv); err != nil {
+		panic(err)
+	}
+}
